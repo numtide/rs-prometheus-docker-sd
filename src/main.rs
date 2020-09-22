ad222872e9c7e8c0350b03814b2f0282bddafd04
Andika Demas Riyandi <andika.riyan@gmail.com>
2020-09-22 23:11:03 +0700

    remove stream, add config path, and read/write file

--- src/main.rs
+++ src/main.rs
@@ -10,50 +10,30 @@ use bollard::Docker;
 use env_logger;
 use std::collections::HashMap;
 use std::default::Default;
+use std::fs;
+use std::fs::OpenOptions;
+use std::time::Duration;
+use std::{thread, time};
 
-use futures_util::stream;
-use futures_util::stream::StreamExt;
 use tokio::runtime::Runtime;
 
 use types::{
     get_config_docker_compose_service, get_config_hostname, get_config_job,
     get_config_metrics_path, get_config_port, get_config_scheme, get_container_hostname,
-    get_container_name, get_scrape_enabled, PContainerInfo,
+    get_container_name, get_scrape_enabled, PromConfig,
 };
 
-async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(unix)]
-    let docker = Docker::connect_with_unix_defaults().unwrap();
-    #[cfg(windows)]
-    let docker = Docker::connect_with_named_pipe_defaults().unwrap();
-
-    let mut list_container_filters = HashMap::new();
-    list_container_filters.insert("status", vec!["running"]);
-
-    let containers = &docker
-        .list_containers(Some(ListContainersOptions {
-            all: true,
-            filters: list_container_filters,
-            ..Default::default()
-        }))
-        .await?;
-
-    // create multiple stream
-    let docker_stream = stream::repeat(docker);
-
-    // zip all the stream with containers' result, then apply each of the containers
-    // with `docker_json_to_promotheus` function
-    docker_stream
-        .zip(stream::iter(containers))
-        .for_each_concurrent(2, docker_json_to_promotheus)
-        .await;
-    Ok(())
-}
+// Maybe_add_docker_info
+//
+// Add the docker information to the prometheus struct, if it has the right label.
 
 // Async function to take running docker's information
 // , and turn into promotheus' json format
-async fn docker_json_to_promotheus(arg: (Docker, &ContainerSummary)) -> () {
-    let (docker, csummary) = arg;
+async fn maybe_add_container_info<'a>(
+    docker: &'a Docker,
+    mut pconfig: PromConfig,
+    csummary: &'a ContainerSummary,
+) -> Result<PromConfig, Box<dyn std::error::Error + 'static>> {
     let container = docker
         .inspect_container(
             csummary.id.as_ref().unwrap(),
@@ -63,54 +43,132 @@ async fn docker_json_to_promotheus(arg: (Docker, &ContainerSummary)) -> () {
         .unwrap();
 
     let empty_hash = HashMap::new();
-    let docker_labels = match csummary.labels.as_ref() {
+    let docker_labels = match csummary.clone().labels {
         Some(x) => x,
-        _ => &empty_hash,
+        _ => empty_hash,
     };
     // let docker_labels = get_config_labels(container_config);
-    let container_name = get_container_name(&container);
-    let mut pcontainer = PContainerInfo::new();
+    let container_name = get_container_name(container.clone());
 
-    pcontainer.labels.job = container_name;
-    pcontainer.labels.name = container_name;
-    pcontainer.labels.id = get_container_hostname(&container);
+    pconfig.labels.job = container_name.clone();
+    pconfig.labels.name = container_name.clone();
+    pconfig.labels.id = get_container_hostname(container.clone());
 
     if !docker_labels.is_empty() {
         match get_scrape_enabled(&docker_labels).unwrap_or(false) {
             true => {
-                let job_name = get_config_job(&docker_labels);
+                let job_name = get_config_job(docker_labels.clone());
                 debug!("Container {} is enabled for prometheus.", container_name);
                 if let true = !job_name.is_empty() {
-                    pcontainer.labels.job = job_name;
+                    pconfig.labels.job = job_name.clone();
                     debug!("Set job name to {}.", job_name)
                 }
                 debug!("Job name is not set, using default value.")
             }
-            false => debug!(
-                "Container {} has no \"prometheus-scrape.enabled\" label and is ignored.",
-                container_name
-            ),
+            false => {
+                debug!(
+                    "Container {} has no \"prometheus-scrape.enabled\" label and is ignored.",
+                    container_name
+                );
+                return Ok(PromConfig::new());
+            }
         }
     } else {
         error!("Docker doesn't have labels")
     }
 
-    let port = get_config_port(&docker_labels);
-    let hostname = get_config_hostname(&docker_labels, container_name);
+    let port = get_config_port(docker_labels.clone());
+    let hostname = get_config_hostname(docker_labels.clone(), container_name.clone());
     let target = format!("{}:{}", hostname, port);
 
-    pcontainer.targets.push(target);
-    pcontainer.labels.scheme = get_config_scheme(&docker_labels);
-    pcontainer.labels.metrics_path = get_config_metrics_path(&docker_labels);
-    pcontainer.labels.com_docker_compose_service =
-        get_config_docker_compose_service(&docker_labels);
-    println!("{:#?}", pcontainer)
+    pconfig.targets.push(target);
+    pconfig.labels.scheme = get_config_scheme(docker_labels.clone());
+    pconfig.labels.metrics_path = get_config_metrics_path(docker_labels.clone());
+    pconfig.labels.com_docker_compose_service =
+        get_config_docker_compose_service(docker_labels.clone());
+
+    if pconfig.targets.len() > 0 {
+        Ok(pconfig)
+    } else {
+        Ok(PromConfig::new())
+    }
+}
+
+async fn run(
+    config_path: &str,
+    refresh_interval_sec: Duration,
+) -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(unix)]
+    let docker = Docker::connect_with_unix_defaults().unwrap();
+    #[cfg(windows)]
+    let docker = Docker::connect_with_named_pipe_defaults().unwrap();
+    let open_config = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(config_path);
+    if let Err(err) = open_config {
+        error!("Error: File error due to {}", err)
+    }
+
+    let mut previous_config = match fs::read_to_string(config_path) {
+        Ok(x) => x,
+        Err(err) => {
+            error!("Error: File error due to {}", err);
+            String::new()
+        }
+    };
+
+    loop {
+        // TODO: Create the empty struct
+        let mut promconfig: Vec<PromConfig> = Vec::new();
+        let pconfig = PromConfig::new();
+
+        // Get the list of containers
+        let mut list_container_filters = HashMap::new();
+        list_container_filters.insert("status", vec!["running"]);
+        let containers = &docker
+            .list_containers(Some(ListContainersOptions {
+                all: true,
+                filters: list_container_filters,
+                ..Default::default()
+            }))
+            .await?;
+
+        // Query each container and add its info to the config if it has the right label
+        for container in containers {
+            let result = maybe_add_container_info(&docker, pconfig.clone(), container).await?;
+            promconfig.push(result)
+        }
+
+        promconfig.retain(|e| !e.targets.is_empty());
+
+        // Only write if the content has changed
+        let current_config = serde_json::to_string(&promconfig)?;
+        if current_config != previous_config {
+            if let Err(err) = fs::write(config_path, current_config.clone()) {
+                error!("Error: File error due to {}", err)
+            }
+            previous_config = current_config;
+        }
+
+        // let result_config = promconfig.iter().filter(|e| e.is_)
+        for pc in promconfig {
+            println!("{:#?}", pc)
+        }
+
+        // Wait for a bit
+        thread::sleep(refresh_interval_sec);
+    }
 }
 
 fn main() {
     env_logger::init();
 
+    let config_path = "/rs-promotheus-docker-sd/docker-targets.json";
+    let refresh_interval_sec = time::Duration::from_secs(900);
+
     let mut rt = Runtime::new().unwrap();
 
-    rt.block_on(run()).unwrap();
+    rt.block_on(run(config_path, refresh_interval_sec)).unwrap();
 }
--- src/types.rs
+++ src/types.rs
@@ -1,55 +1,56 @@
+use serde::{Deserialize, Serialize};
 use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
 use std::default::Default;
 
 use bollard::models::ContainerInspectResponse;
 
-#[derive(Debug)]
-pub struct PContainerLabel<'a> {
-    pub job: &'a str,
-    pub name: &'a str,
-    pub id: &'a str,
-    pub scheme: &'a str,
-    pub metrics_path: &'a str,
-    pub com_docker_compose_service: &'a str,
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct PromConfigLabel {
+    pub job: String,
+    pub name: String,
+    pub id: String,
+    pub scheme: String,
+    pub metrics_path: String,
+    pub com_docker_compose_service: String,
 }
 
-impl<'a> Default for PContainerLabel<'a> {
+impl Default for PromConfigLabel {
     fn default() -> Self {
-        PContainerLabel {
-            job: "",
-            name: "",
-            id: "",
-            scheme: "",
-            metrics_path: "",
-            com_docker_compose_service: "",
+        PromConfigLabel {
+            job: String::from(""),
+            name: String::from(""),
+            id: String::from(""),
+            scheme: String::from(""),
+            metrics_path: String::from(""),
+            com_docker_compose_service: String::from(""),
         }
     }
 }
 
-impl<'a> PContainerLabel<'a> {
+impl PromConfigLabel {
     pub fn new() -> Self {
         Default::default()
     }
 }
 
-#[derive(Debug)]
-pub struct PContainerInfo<'a> {
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct PromConfig {
     // labels for container information
-    pub labels: PContainerLabel<'a>,
+    pub labels: PromConfigLabel,
     pub targets: Vec<String>,
 }
 
-impl<'a> PContainerInfo<'a> {
+impl PromConfig {
     pub fn new() -> Self {
         Default::default()
     }
 }
 
-impl<'a> Default for PContainerInfo<'a> {
+impl Default for PromConfig {
     fn default() -> Self {
-        PContainerInfo {
-            labels: PContainerLabel::new(),
+        PromConfig {
+            labels: PromConfigLabel::new(),
             targets: Vec::new(),
         }
     }
@@ -61,54 +62,54 @@ pub fn get_scrape_enabled(hash: &HashMap<String, String, RandomState>) -> Option
     enabled_label.map(|e| e.eq(&String::from("true")))
 }
 
-pub fn get_config_job(hash: &HashMap<String, String, RandomState>) -> &str {
+pub fn get_config_job(hash: HashMap<String, String, RandomState>) -> String {
     let config_job = &"prometheus-scrape.job_name".to_string();
     if let Some(job) = hash.get(config_job) {
-        return job;
+        return String::from(job);
     }
-    return "";
+    return String::from("");
 }
 
-pub fn get_config_port(hash: &HashMap<String, String, RandomState>) -> &str {
+pub fn get_config_port(hash: HashMap<String, String, RandomState>) -> String {
     let config_port = &String::from("prometheus-scrape.port");
-    let port = "9090";
+    let port = String::from("9090");
     if let Some(new_port) = hash.get(config_port) {
         debug!("Port is set to {}.", new_port);
-        return new_port;
+        return String::from(new_port);
     }
     debug!("Job name is not set, using default value.");
     return port;
 }
 
-pub fn get_config_scheme(hash: &HashMap<String, String, RandomState>) -> &str {
+pub fn get_config_scheme(hash: HashMap<String, String, RandomState>) -> String {
     let config_port = &String::from("prometheus-scrape.scheme");
-    let scheme = "http";
+    let scheme = String::from("http");
     if let Some(new_scheme) = hash.get(config_port) {
         debug!("Port is set to {}.", new_scheme);
-        return new_scheme;
+        return String::from(new_scheme);
     }
     debug!("Job name is not set, using default value.");
     return scheme;
 }
 
-pub fn get_config_metrics_path(hash: &HashMap<String, String, RandomState>) -> &str {
+pub fn get_config_metrics_path(hash: HashMap<String, String, RandomState>) -> String {
     let config_port = &String::from("prometheus-scrape.metrics_path");
-    let metrics_path = "/metrics";
+    let metrics_path = String::from("/metrics");
     if let Some(new_path) = hash.get(config_port) {
         debug!("Port is set to {}.", new_path);
-        return new_path;
+        return String::from(new_path);
     }
     debug!("Job name is not set, using default value.");
-    return metrics_path;
+    return String::from(metrics_path);
 }
 
-pub fn get_config_docker_compose_service(hash: &HashMap<String, String, RandomState>) -> &str {
+pub fn get_config_docker_compose_service(hash: HashMap<String, String, RandomState>) -> String {
     let config_port = &String::from("com.docker.compose.service");
     // TODO: Find out what is compose_service's default, given that it is not in documentation
-    let compose_service = "";
+    let compose_service = String::from("");
     if let Some(new_compose_service) = hash.get(config_port) {
         debug!("Compose service name is set to {}.", new_compose_service);
-        return new_compose_service;
+        return String::from(new_compose_service);
     }
     debug!("Job name is not set, using default value.");
     return compose_service;
@@ -120,21 +121,18 @@ pub fn get_config_docker_compose_service(hash: &HashMap<String, String, RandomSt
 // 2. `prometheus-scrape.ip_as_hostname`
 // `promotheus-scrape.hostname` is preferred first, and then `prometheus-scrape.ip_as_hostname`
 // in case both are not detected, the default container's name will be used.
-pub fn get_config_hostname<'a>(
-    hash: &'a HashMap<String, String, RandomState>,
-    cname: &'a str,
-) -> &'a str {
+pub fn get_config_hostname(hash: HashMap<String, String, RandomState>, cname: String) -> String {
     let config_hostname = &String::from("prometheus-scrape.hostname");
     let config_ip_hostname = &String::from("prometheus-scrape.ip_as_hostname");
-    if let Some(new_hostname) = &hash.get(config_hostname) {
+    if let Some(new_hostname) = hash.get(config_hostname) {
         debug!("Hostname is set to {}.", new_hostname);
-        return new_hostname;
+        return String::from(new_hostname);
     }
     if let Some(new_ip_hostname) = hash.get(config_ip_hostname) {
         match new_ip_hostname.eq(&String::from("true")) {
             true => {
                 debug!("IP address for hostname is set to {}.", new_ip_hostname);
-                return &new_ip_hostname;
+                return String::from(new_ip_hostname);
             }
             false => {
                 debug!("hostname is not set, using default value.");
@@ -147,16 +145,19 @@ pub fn get_config_hostname<'a>(
     return cname;
 }
 
-pub fn get_container_name(ctr: &ContainerInspectResponse) -> &str {
-    match &ctr.name {
-        Some(x) => x.strip_prefix("/").unwrap_or(""),
-        _ => "",
+pub fn get_container_name(ctr: ContainerInspectResponse) -> String {
+    match ctr.name {
+        Some(x) => x
+            .strip_prefix("/")
+            .map(|e| String::from(e))
+            .unwrap_or(String::from("")),
+        _ => String::from(""),
     }
 }
 
-pub fn get_container_hostname(ctr: &ContainerInspectResponse) -> &str {
-    match ctr.config.as_ref().and_then(|e| e.hostname.as_ref()) {
-        Some(x) => return &x,
-        _ => "",
+pub fn get_container_hostname(ctr: ContainerInspectResponse) -> String {
+    match ctr.config.and_then(|e| e.hostname) {
+        Some(x) => return x,
+        _ => String::from(""),
     }
 }
